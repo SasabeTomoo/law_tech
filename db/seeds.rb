f2986ba84b368db268d5d65dd6d59356db4598da
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-10-17 18:03:05 +0900

    通常損耗等の具体例の検索機能を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 Sample.create!(
   category: "1.床（畳、フローリング、カーペットなど）",
-  field: "通常損耗",
+  field: "usual",
   content: "フローリングワックスがけ"
 )
 
 Sample.create!(
   category: "1.床（畳、フローリング、カーペットなど）",
-  field: "超過損耗",
+  field: "unusual",
   content: "たばこによるカーペット損傷"
 )
 
 Sample.create!(
   category: "4.その他",
-  field: "通常損耗",
+  field: "usual",
   content: "鍵の交換（紛失・毀損なし）"
 )
