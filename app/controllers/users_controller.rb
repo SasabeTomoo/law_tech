e8d411452adddcf63fe9b16e0d7f023e6f9ed406
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-10-21 19:20:47 +0900

    フロントの画像インプット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to explain1_path(@user.id)
+      session[:user_id] = @user.id
     else
       render :new
     end
