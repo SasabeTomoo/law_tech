d26d586e84f32a923a6e165ff695f4fbfd37e748
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-10-24 13:49:39 +0900

    adjustment.rbのバリデーション消去

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -3,10 +3,4 @@ class Adjustment < ApplicationRecord
   has_many :datails, dependent: :destroy
   accepts_nested_attributes_for :datails
 
-
-  # 1. 工事が1つも登録されていない
-  validate :require_any_datail
-  def require_any_datail
-    errors.add(:base, :no_datail) if datails.blank?
-  end
 end
